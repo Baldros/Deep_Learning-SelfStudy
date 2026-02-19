0fe74da51de3f2d3ab8e26344e3f6e6106c98921
André <andreamorim@deepmath.tech>
2026-02-19 11:26:03 -0300

    Reestruturação do repositório

---
+++ Neural Networks/MINIST Classifier/MINISTClassifier.py
@@ -0,0 +1,9 @@
+"""
+O objetivo desse código é trabalhar a construção de um
+um uma rede neural capaz de classificar dados dados do
+conjunto de dados MINIST usando apenas o NumPy.
+"""
+
+# Dependências
+import numpy as np
+
